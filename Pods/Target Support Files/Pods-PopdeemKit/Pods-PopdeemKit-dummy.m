c43bd510928917c2dcda066c5cd4a0e3205d31ce
NQuinn27 <niall.quinn.27@gmail.com>
2017-03-02 17:13:16 +0000

    .gitignore cleanup

--- Pods/Target Support Files/Pods-PopdeemKit/Pods-PopdeemKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PopdeemKit : NSObject
-@end
-@implementation PodsDummy_Pods_PopdeemKit
-@end
--- Pods/Target Support Files/PopdeemKit/PopdeemKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PopdeemKit : NSObject
-@end
-@implementation PodsDummy_PopdeemKit
-@end
